a7599a9ace6fee6b44c8bc6a8b899f9252793de2
John Doe <johndoe@example.com>
2021-03-14 19:51:03 +0300

    created field array

---
+++ src/engine/1_phase_field_generation/field.js
@@ -0,0 +1,15 @@
+export default class Field {
+    constructor(rows = 10, cols = 10, colors = ['red', 'green', 'blue', 'orange', 'yellow']){
+		this.rows = rows
+        this.cols = cols
+        this.colors = colors
+        
+
+        this.cells = [...new Array(cols)].map((column, columnIndex) =>{ return [...new Array(10)].map((row, rowIndex) => {return {x: rowIndex, y: columnIndex, color: colors[Math.floor(Math.random() * colors.length)]}})})
+        console.log(this.cells)
+        
+
+    }
+}
+
+let field = new Field(10,10)
